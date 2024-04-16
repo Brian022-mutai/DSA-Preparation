3feecf37753412b2c920297d24b90b87f20ca7fa
Calebbii <biicaleb316@gmail.com>
2024-04-16 23:29:28 +0300

    adds a function that checks the nearest duplicate of an array

---
+++ Hashmap/inspectes_duplicate.py
@@ -0,0 +1,9 @@
+def containsNearbyDuplicate(self,nums, k: int) -> bool:
+        num_index = {}
+
+        for i, num in enumerate(nums):
+            if num in num_index and i - num_index[num] <= k:
+                return True
+            num_index[num] = i
+
+        return False
